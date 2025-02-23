354acbe0e95cac3699f882e6c20ec09d4c6c9568
Александр Чернавин <djoreh64@yandex.ru>
2025-02-23 20:28:39 +0300

    feat: add opengraph metatags

--- src/app/cart/layout.tsx
+++ src/app/cart/layout.tsx
@@ -3,7 +3,35 @@ import { FC, PropsWithChildren } from "react";
 
 export const metadata: Metadata = {
   title: "Корзина | OZON",
-  description: "OZON",
+  description:
+    "Ознакомьтесь с товарами, которые вы добавили в корзину на OZON. Выберите способ оплаты и оформления доставки.",
+  openGraph: {
+    title: "Корзина | OZON",
+    description:
+      "Ознакомьтесь с товарами, которые вы добавили в корзину на OZON. Выберите способ оплаты и оформления доставки.",
+    url: "https://djoreh64.ru/cart",
+    siteName: "OZON",
+    images: [
+      {
+        url: "https://djoreh64.ru/images/logo.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Корзина на OZON",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Корзина | OZON",
+    description:
+      "Ознакомьтесь с товарами, которые вы добавили в корзину на OZON. Выберите способ оплаты и оформления доставки.",
+    images: ["https://djoreh64.ru/images/logo.jpg"],
+    creator: "@Ozon",
+  },
+  robots: "index, follow",
+  keywords: "OZON, корзина, покупки онлайн, товары, доставка, оплата",
+  authors: [{ name: "OZON" }],
 };
 
 const Layout: FC<PropsWithChildren> = ({ children }) => {
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,9 +4,42 @@ import type { Metadata } from "next";
 import { PropsWithChildren } from "react";
 import { Toaster } from "react-hot-toast";
 
+export const viewport = {
+  width: "device-width",
+  initialScale: 1.0,
+};
+
 export const metadata: Metadata = {
   title: "Главная | OZON",
-  description: "OZON",
+  description:
+    "OZON - крупнейший онлайн-магазин, предлагающий товары для всех категорий покупателей. Отличные предложения и скидки!",
+  openGraph: {
+    title: "Главная | OZON",
+    description:
+      "OZON - крупнейший онлайн-магазин, предлагающий товары для всех категорий покупателей. Отличные предложения и скидки!",
+    url: "https://djoreh64.ru",
+    siteName: "OZON",
+    images: [
+      {
+        url: "https://djoreh64.ru/images/logo.jpg",
+        width: 1200,
+        height: 630,
+        alt: "OZON - Онлайн магазин",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Главная | OZON",
+    description:
+      "OZON - крупнейший онлайн-магазин, предлагающий товары для всех категорий покупателей. Отличные предложения и скидки!",
+    images: ["https://djoreh64.ru/images/logo.jpg"],
+    creator: "@Ozon",
+  },
+  robots: "index, follow",
+  keywords: "OZON, интернет-магазин, товары, скидки, покупки онлайн",
+  authors: [{ name: "OZON" }],
 };
 
 interface Props {
