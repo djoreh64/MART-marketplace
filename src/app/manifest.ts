227158a92baee5e1d37abbf39f99db5eb9cb4f20
Александр Чернавин <djoreh64@yandex.ru>
2025-02-26 02:23:13 +0300

    fix: fix conflicts

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -15,40 +15,19 @@ export default function manifest(): MetadataRoute.Manifest {
         src: "/icon-192x192.png",
         sizes: "192x192",
         type: "image/png",
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
         purpose: "maskable",
->>>>>>> 5682a3241b38baab74d9077cbfc3c6ba51cb5b93
-=======
-        purpose: "maskable",
->>>>>>> b8a8d18e57ccd3623150479fa5c6bb07fe2f3179
       },
       {
         src: "/icon-256x256.png",
         sizes: "256x256",
         type: "image/png",
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
         purpose: "maskable",
->>>>>>> 5682a3241b38baab74d9077cbfc3c6ba51cb5b93
-=======
-        purpose: "maskable",
->>>>>>> b8a8d18e57ccd3623150479fa5c6bb07fe2f3179
       },
       {
         src: "/icon-512x512.png",
         sizes: "512x512",
         type: "image/png",
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
         purpose: "maskable",
->>>>>>> 5682a3241b38baab74d9077cbfc3c6ba51cb5b93
-=======
-        purpose: "maskable",
->>>>>>> b8a8d18e57ccd3623150479fa5c6bb07fe2f3179
       },
     ],
   };
